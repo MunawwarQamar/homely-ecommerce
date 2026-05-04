2a95ae7909bae9b28012795a7ea9b0fa355846af
MunawwarQamar <Munawwarbaba2004@gmail.com>
2026-05-04 15:05:54 +0300

    Prepare project for deployment

--- homely_project/homely_project/settings.py
+++ homely_project/homely_project/settings.py
@@ -26,8 +26,7 @@ SECRET_KEY = 'django-insecure-6fs@wnetk6q5zwcuwa+@4%f-oo1ng9k31r+xbqb9$@ujjf2(lw
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
@@ -122,7 +121,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
