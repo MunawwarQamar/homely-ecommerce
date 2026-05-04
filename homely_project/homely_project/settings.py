037613baa5a53eb40b31a2c7b5e94bb59e811783
MunawwarQamar <Munawwarbaba2004@gmail.com>
2026-05-04 15:51:34 +0300

    Add database

--- homely_project/homely_project/settings.py
+++ homely_project/homely_project/settings.py
@@ -75,6 +75,17 @@ WSGI_APPLICATION = 'homely_project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': 'homely_db',
+#         'USER': 'root',
+#         'PASSWORD': '123456',
+#         'HOST': 'localhost',
+#         'PORT': '3306',
+#     }
+# }
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
